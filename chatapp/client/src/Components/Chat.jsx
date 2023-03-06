cdcf9f9d9ad9e4413f435609398981eea196be96
Axel Stahl <axelkeizo@gmail.com>
2023-03-06 02:50:08 -0500

    chat component

---
+++ chatapp/client/src/Components/Chat.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import { useEffect } from "react";
+
+export default function Chat({ socket }) {
+
+    function sendmessage(e) {
+        e.preventDefault();
+        var form = document.getElementById("form");
+        var input = document.getElementById("input");
+        if (input.value) {
+            socket.emit('chat message', input.value);
+            input.value = '';
+        }
+    };
+
+    useEffect(() => {
+        socket.on('back message', function(msg) {
+            var messages = document.getElementById("messages");
+            var item = document.createElement('li');
+            var message = msg.message;
+            var id = msg.id;
+            item.innerHTML = `<em>${id}:</em>  ${message}`;
+            messages.appendChild(item);
+            window.scrollTo(0, document.body.scrollHeight);
+        });
+    });
+
+    
+    return (
+    <div>
+        <ul id="messages"></ul>
+        <form id="form" onSubmit={sendmessage}>
+            <input id="input" autoComplete="off" type="text" />
+            <button type="submit">Send</button>
+        </form>
+    </div>
+    );
+};
