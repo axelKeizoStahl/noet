e77964368be087cb316284266926326ab083e785
Axel Stahl <axelkeizo@gmail.com>
2023-03-06 22:41:16 -0500

    added react-router-dom

--- chatapp/client/src/App.jsx
+++ chatapp/client/src/App.jsx
@@ -1,15 +1,23 @@
 import React from "react";
 import Chat from "./Components/Chat";
+import Home from "./Components/Home";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import socketIO from 'socket.io-client';
 
 const socket = socketIO.connect("http://localhost:4000");
 
 function App() {
   return (
-    <div>
-      <Chat socket={socket}/>
-    </div>
+    <BrowserRouter>
+      <div>
+          <Routes>
+            <Route path="/" element={<Home socket={socket} />}></Route>
+            <Route path="/chat" element={<Chat socket={socket} />}></Route>
+            <Route path="/room" element={<Room socket={socket} />}></Route>
+          </Routes>
+      </div>
+    </BrowserRouter>
   );
-}
+};
 
 export default App;
