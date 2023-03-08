2f623ef3969a77c402865f9ff0f758064cd78110
Axel Stahl <axelkeizo@gmail.com>
2023-03-08 01:53:06 -0500

    no room page

--- chatapp/client/src/App.jsx
+++ chatapp/client/src/App.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import Chat from "./Components/Chat";
-import Home from "./Components/Home";
-import Room from "./Components/Room";
+import Login from "./Components/Login";
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import socketIO from 'socket.io-client';
 
@@ -12,9 +11,8 @@ function App() {
     <BrowserRouter>
       <div>
           <Routes>
-            <Route path="/" element={<Home socket={socket} />}></Route>
+            <Route path="/" element={<Login socket={socket} />}></Route>
             <Route path="/chat" element={<Chat socket={socket} />}></Route>
-            <Route path="/room" element={<Room socket={socket} />}></Route>
           </Routes>
       </div>
     </BrowserRouter>
