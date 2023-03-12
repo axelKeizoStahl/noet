ede46bfdb65b9b6963fd5406302b9ee5c407e44d
Axel Stahl <axelkeizo@gmail.com>
2023-03-12 17:41:27 -0400

    changed message commit

--- chatapp/client/src/Components/Login.jsx
+++ chatapp/client/src/Components/Login.jsx
@@ -3,13 +3,16 @@ import { useNavigate } from "react-router-dom";
 
 export default function Home({ socket }) {
     const navigate = useNavigate();
-    const [name, setName] = useState('');
-    const [room, setRoom] = useState('');
+
 
     const handleSubmit = (e) => {
         e.preventDefault();
+        var login;
         //localStorage.setUserName('userName', userName);
-        socket.emit('newUser', ({name, room}));
+        socket.emit('newUser', login = {
+            username: document.getElementById("username").value,
+            room: document.getElementById("chatroom").value
+        });
         navigate('/chat')
         console.log(socket.id)
     };
@@ -21,16 +24,12 @@ export default function Home({ socket }) {
                 type="text"
                 minLength="6"
                 id="username"
-                value={name}
-                onChange={(e) => setName(e.target.value)}
             />
             <label htmlFor="username">Chat Room</label>
             <input 
                 type="text"
                 minLength="6"
                 id="chatroom"
-                value={room}
-                onChange={(e) => setRoom(e.target.value)}
             />
             <button id="signin" type="submit">Sign In</button>
         </form>
